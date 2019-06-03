9e65b1699627eb23e9ea01d7cca068f00c94450f
Mai Tien <maitien2004@xuongrong.info>
2019-06-03 17:09:16 +0700

    Update prereqs-ubuntu.sh

--- prereqs-ubuntu.sh
+++ prereqs-ubuntu.sh
@@ -36,7 +36,7 @@ sudo usermod -aG docker $(whoami)
 
 # Install docker compose
 echo "# Installing Docker-Compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
